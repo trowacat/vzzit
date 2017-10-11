0ddc9135323689c85a21500e6b6f81c319f2a754
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-10-10 22:40:59 -0400

    fixed link button

--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -6,7 +6,7 @@ $('.btn-shorten').on('click', function () {
     dataType: 'JSON',
     data: { url: $('#url-field').val() },
     success: function (data) {
-      var resultHTML = '<a class="result"  value="' + data.shortUrl + '" href="' + data.shortUrl + '">'
+      var resultHTML = '<a class="result"  value="' + data.shortUrl + '" href="https://' + data.shortUrl + '">'
         + data.shortUrl + '</a> <button type="button" class="btn" data-clipboard-text="' + data.shortUrl + '">' +
         '<img src="assets/clippy.svg" width="12"> Copy to clipboard' +
           '</button>';

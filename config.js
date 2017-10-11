ddd5e5ccb1889d6f2b349edc8eeb2eb803b0da25
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-10-10 21:18:39 -0400

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = '159.203.18.26';
+config.webhost = 'http://vzzit.me';
 
 config.db.host = 'mongodb://shorturl:Zaqmmkkl2@ds115035.mlab.com:15035';
 config.db.name = 'shorturl';

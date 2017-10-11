663f84263143556a183649d3f672bf0e9ef2a8a6
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-10-10 21:22:14 -0400

    add slash i forgot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'http://vzzit.me';
+config.webhost = 'http://vzzit.me/';
 
 config.db.host = 'mongodb://shorturl:Zaqmmkkl2@ds115035.mlab.com:15035';
 config.db.name = 'shorturl';

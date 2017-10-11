bedf2466956f79ed8d660c9408ac9ac79731e04b
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-10-10 23:15:57 -0400

    fixed http problems

--- app.js
+++ app.js
@@ -14,26 +14,36 @@ var Url = require('./models/url');
 
 mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
 
+// check link function
+function formatUrl(url)
+{
+    var httpString = "http://";
+    var httpsString = "https://";
+    if (url.substr(0, httpString.length).toLowerCase() !== httpString && url.substr(0, httpsString.length).toLowerCase() !== httpsString)
+                url = httpString + url;
+    return url;
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.post('/api/shorten', function(req, res){
+app.post('/api/shorten', function (req, res) {
   var longUrl = req.body.url;
   var shortUrl = '';
 
   // check if url already exists in database
-  Url.findOne({long_url: longUrl}, function (err, doc){
-    if (doc){
+  Url.findOne({ long_url: longUrl }, function (err, doc) {
+    if (doc) {
       shortUrl = config.webhost + base58.encode(doc._id);
 
       // the document exists, so we return it without creating a new entry
-      res.send({'shortUrl': shortUrl});
+      res.send({ 'shortUrl': shortUrl });
     } else {
       // since it doesn't exist, let's go ahead and create it:
       var newUrl = Url({
@@ -41,14 +51,14 @@ app.post('/api/shorten', function(req, res){
       });
 
       // save the new link
-      newUrl.save(function(err) {
-        if (err){
+      newUrl.save(function (err) {
+        if (err) {
           console.log(err);
         }
 
         shortUrl = config.webhost + base58.encode(newUrl._id);
 
-        res.send({'shortUrl': shortUrl});
+        res.send({ 'shortUrl': shortUrl });
       });
     }
 
@@ -56,16 +66,16 @@ app.post('/api/shorten', function(req, res){
 
 });
 
-app.get('/:encoded_id', function(req, res){
+app.get('/:encoded_id', function (req, res) {
 
   var base58Id = req.params.encoded_id;
 
   var id = base58.decode(base58Id);
 
   // check if url already exists in database
-  Url.findOne({_id: id}, function (err, doc){
+  Url.findOne({ _id: id }, function (err, doc) {
     if (doc) {
-      res.redirect(doc.long_url);
+        res.redirect(formatUrl(doc.longUrl));
     } else {
       res.redirect(config.webhost);
     }
@@ -73,6 +83,10 @@ app.get('/:encoded_id', function(req, res){
 
 });
 
-var server = app.listen(HTTP_PORT, function(){
+app.get('', function (req, res) {
+  res.redirect('/');
+})
+
+var server = app.listen(HTTP_PORT, function () {
   console.log('Server listening on port: ' + HTTP_PORT);
 });
--- public/javascripts/shorten.js
+++ public/javascripts/shorten.js
@@ -1,19 +1,19 @@
 $('.btn-shorten').on('click', function () {
-
-  $.ajax({
-    url: '/api/shorten',
-    type: 'POST',
-    dataType: 'JSON',
-    data: { url: $('#url-field').val() },
-    success: function (data) {
-      var resultHTML = '<a class="result" ' + '" href="https://' + data.shortUrl + '">'
-        + data.shortUrl + '</a> <button type="button" class="btn" data-clipboard-text="' + data.shortUrl + '">' +
-        '<img src="assets/clippy.svg" width="12"> Copy to clipboard' +
-          '</button>';
-      $('#link').html(resultHTML);
-      $('#link').hide().fadeIn('slow');
-    }
+  
+    $.ajax({
+      url: '/api/shorten',
+      type: 'POST',
+      dataType: 'JSON',
+      data: { url: $('#url-field').val() },
+      success: function (data) {
+        var resultHTML = '<a class="result" ' + '" href="https://' + data.shortUrl + '">'
+          + data.shortUrl + '</a> <button type="button" class="btn" data-clipboard-text="' + data.shortUrl + '">' +
+          '<img src="assets/clippy.svg" width="12"> Copy to clipboard' +
+            '</button>';
+        $('#link').html(resultHTML);
+        $('#link').hide().fadeIn('slow');
+      }
+    });
+  
   });
-
-});
-
+  
